e852a30c52af01ffe3682f564dac494a07542c83
Vkumars <vishalkumars.work@gmail.com>
2025-09-24 16:52:48 +0530

    Updated CRON job timing variables from hardcoded to application configuration file

--- tasks-service/app/jobs/TaskScheduler.scala
+++ tasks-service/app/jobs/TaskScheduler.scala
@@ -5,15 +5,18 @@ import javax.inject._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import services.TaskService
+import play.api.Configuration
 
 
 @Singleton
-class TaskScheduler @Inject()(taskService: TaskService, actorSystem: ActorSystem)(implicit ec: ExecutionContext) {
+class TaskScheduler @Inject()(taskService: TaskService, actorSystem: ActorSystem, configuration: Configuration)(implicit ec: ExecutionContext) {
 
+  private val initialDelay: FiniteDuration = configuration.get[FiniteDuration]("jobs.TaskScheduler.initialDelay")
+  private val interval: FiniteDuration = configuration.get[FiniteDuration]("jobs.TaskScheduler.interval")
 
   actorSystem.scheduler.scheduleAtFixedRate(
-    initialDelay = 0.seconds,
-    interval = 1.minute
+    initialDelay = initialDelay,
+    interval = interval
   ) {
     () =>
       taskService.processDueTasks()
